012c2c6841c53321f1e60e984fec71dfd7401061
youngin3737 <youngin3737@gm.gist.ac.kr>
2024-12-14 11:35:57 +0900

    impulse model and bear firmware

--- README.md
+++ README.md
@@ -15,3 +15,22 @@ npm run dev
 # Repository Structure
 Controllers: Handles the background logic for the actions
 Routes: Handles all endpoint routes that are available from the server
+Models: All edge-impulse model for state classification
+Firmware: Micropython codes for data collection
+
+# Interact with Bear!
+**IDLE**: The bear remains stationary, sitting upright without movement.
+**HANDSHAKING**: Hold the bear's left hand and shake it gently up and down.
+**CLAPPING**: Grab both of the bear's hands and softly clap them together in front.
+**SHAKING**: Gently shake the bear by moving it slightly, about 3 cm is enough for recognition (avoid shaking too hard).
+**PETTING**: Lightly tap (not stroking) repeatedly the sensor on the bear’s head with your hand.
+
+# Serial Connection with Bear~
+- A serial port can only be accessed by one channel at a time. 
+- If you encounter an "access denied" error when running your program, check whether the port is already in use by another program, such as Edge Impulse or VS Code, and disconnect it if necessary.
+
+
+
+
+
+
---
+++ firmware/main.py
@@ -0,0 +1,42 @@
+from imu import MPU6050 
+from bmp085 import BMP180
+from vl6180x import Sensor
+import time, math, utime
+from machine import Pin, I2C, ADC
+i2c = I2C(1, scl=Pin(11), sda=Pin(10))
+print("I2C devices found:", i2c.scan())
+i2c_imu = I2C(0, scl=Pin(21), sda=Pin(20))
+print("I2C devices found:", i2c.scan(), i2c_imu.scan())
+
+bmp = BMP180(i2c)
+
+imu = MPU6050(i2c_imu)
+tof_sensor = Sensor(i2c_imu)
+flex_sensor = ADC(Pin(27))
+bmp.sealevel = 101325
+
+basis = bmp.pressure
+
+
+while True:
+
+    flex_val = flex_sensor.read_u16()
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    gx=round(imu.gyro.x)
+    gy=round(imu.gyro.y)
+    gz=round(imu.gyro.z)
+    tem=round(imu.temperature,2)
+    dist = tof_sensor.range()
+    pres_hPa = bmp.pressure - basis
+    altitude = bmp.altitude
+    tempC = bmp.temperature
+
+    #print("ax",ax,"\tay",ay,"\taz",az,"\tpressure",pres_hPa,"\tbeats", beats, "\tflex",flex_val,"\ttouch",touch_val,"\t",end="\r")
+    #print("pressure", pres_hPa, "\taccel_x", ax, "\tflex",flex_val,"\ttouch", dist)
+    print(pres_hPa, ax, ay, ax, flex_val, dist)
+    #print("pressure",pres_hPa,"\t", "beats", beats, raw_value, "\t",end="\r")
+    #print("pressure",pres_hPa)
+
+    time.sleep(0.03)
